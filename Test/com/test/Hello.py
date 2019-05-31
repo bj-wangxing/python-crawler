dd9d322eba3683117b0d799ba6e12fe06000a42e
13641900178 <13641900178@DESKTOP-ER3R5IF>
2019-05-31 10:01:04 +0800

    淘宝MM图片抓取

--- Test/com/test/Hello.py
+++ Test/com/test/Hello.py
@@ -1,11 +1,42 @@
+class Vector:
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+ 
+   def __str__(self):
+      return 'Vector (%d, %d)' % (self.a, self.b)
+   
+   def __add__(self,other):
+      return Vector(self.a + other.a, self.b + other.b)
+ 
+   def __sub__(self,other):
+       return Vector(self.a - other.a, self.b - other.b)
+   
+v1 = Vector(2,10)
+v2 = Vector(5,-2)
+print v1 - v2
+
+#python 100 for 1
+def test1():
+    for i in range(1,5):
+        for j in range(1,5):
+            for k in range(1,5):
+                if( i != k ) and (i != j) and (j != k):
+                    print i,j,k
+                    
+#test1()
 
 
-def hw(n):
-    if n==1:
-        return 1
-    else:
-        return 2*n
-    
-print(hw(2));
-
-    
+def test2():
+    i = int(raw_input('test'))
+    arr = [1000000,600000,400000,200000,100000,0]
+    rat = [0.01,0.015,0.03,0.05,0.075,0.1]
+    r = 0
+    for idx in range(0,6):
+        if i>arr[idx]:
+            r+=(i-arr[idx])*rat[idx]
+            print (i-arr[idx])*rat[idx]
+            i=arr[idx]
+    print r
+   
+test2()           
